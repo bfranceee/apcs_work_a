3f48705da1936c15ac9782e2a5b13a7731beed2d
bfranceee <brileydfrance@gmail.com>
2024-09-11 09:36:35 -0700

    multiplesof3

---
+++ unit0/Loops.java
@@ -0,0 +1,16 @@
+
+public class Loops
+{
+    public static void main(String[] args) 
+    {
+        for (int i=0; i<10; i++)
+        {
+            if(1%2 == 0)
+            {
+                System.out.println("Even");
+            } else {
+           System.out.println(i); 
+            }
+        }
+    }
+}
---
+++ unit0/Multiplesof3.java
@@ -0,0 +1,15 @@
+public class Multiplesof3
+    (
+    public static void main(String[] args) 
+    {
+        System.out.println("Multiples of 3");
+        System.out.println();
+        //print multiples of 3
+        for (int i=0; i<10; i++)
+        {
+            if(i%3 == 0)
+                System.out.println(i);
+        }
+    }
+
+)
