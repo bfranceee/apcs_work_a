23273dba055d77ff18fc92a58798a7d47fd9b66c
bfranceee <brileydfrance@gmail.com>
2024-09-16 14:03:17 -0700

    Functions

--- unit0/Functions.java
+++ unit0/Functions.java
@@ -10,5 +10,12 @@ public class Functions {
     
     public static void main(String[] args){
         hello();
+        
+        double a = 5.0;
+        double b = 7.0;
+        System.out.println("a+b: " + sum(a,b));
+        
+        double c = sum(6.23,7.34);
+        System.out.println("a+b: " + c)
     }
 }
