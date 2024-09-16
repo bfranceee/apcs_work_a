662ba739729eda44a3097ea8eee330701f7d2da4
bfranceee <brileydfrance@gmail.com>
2024-09-16 13:59:26 -0700

    Functions

---
+++ unit0/Functions.java
@@ -0,0 +1,14 @@
+//Functions.java
+
+
+public class Functions {
+    
+    
+    public static void hello(){
+        System.out.println("Hello, world!");
+    }
+    
+    public static void main(String[] args){
+        hello();
+    }
+}
