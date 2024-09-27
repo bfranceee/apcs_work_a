7a0be54942dafe39903c477cc1721785d34e894d
bfranceee <brileydfrance@gmail.com>
2024-09-27 14:57:08 -0700

    ic

--- unit0/FizzBuzz.java
+++ unit0/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 30; i++) {
+        for (int i = 1; i < 30; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } 
---
+++ unit0/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        for (int i = 1; i < 30; i++) {
+            a = a*3;
+            System.out.println(a);
+    }
+}
+}
