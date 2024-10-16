f081018de609dd1700ae88e2c64e89d4c03850a9
bfranceee <brileydfrance@gmail.com>
2024-10-16 13:18:32 -0700

    coinflip

---
+++ unit3/coinFlip.java
@@ -0,0 +1,22 @@
+public class coinFlip {
+    
+    
+    public static void coinFlip()
+    {
+        int value = (int) (Math.random() * 10 + 1);
+            
+        if (value % 2 == 0)
+            System.out.println("Heads");
+        else
+            System.out.println("Tails");
+        
+    }
+    
+    public static void main(String [] args){
+        for (int i = 0; i <= 20; i++) {
+            coinFlip();
+            
+        }
+        
+    }
+}
