512319f08c39fa919f1f47d97f8b1f14892b9c64
bfranceee <brileydfrance@gmail.com>
2024-10-31 14:36:17 -0700

    unit4

--- unit4/countOccurrences.java
+++ unit4/countOccurrences.java
@@ -1,16 +1,10 @@
-public class CountOccurrences {
+public class countOccurrences {
     public static int countLetters(String str, String target) {
         int total = 0;
-        int index = 0;
-
-        while (index < str.length()) {
-            index = str.indexOf(target, index);
-            if (index != -1) {
-                total++;
-                index += target.length();
-            } else {
-                break;
-            }
+        for (int i = 0; i <= str.length() - target.length(); i++) {
+            String sub = str.substring(i, i + target.length());
+            if (sub.equals(target))
+            total += 1;
         }
         return total;
     }
@@ -30,8 +24,9 @@ public class CountOccurrences {
 
     public static void main(String[] args) {
         testcountLetters("Mississippi", "iss", 2);
-        testcountLetters("banananana", "na", 3);
-        testcountLetters("schooooooool", "oo", 4);
+        testcountLetters("banananana", "na", 4);
+        testcountLetters("chickchickchickchickchick", "chick", 5);
         testcountLetters("Brileyeyeyeyeyey", "ey", 6);
+        testcountLetters("schooooool", "oo", 5);
     }
 }
