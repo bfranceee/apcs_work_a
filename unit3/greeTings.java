2ef31cd8b4df7d5f1b03ad7304ad88be72c935ee
bfranceee <brileydfrance@gmail.com>
2024-11-03 12:01:20 -0800

    y

--- unit3/greeTings.java
+++ unit3/greeTings.java
@@ -1,12 +1,11 @@
 public class greeTings {
-    public static void testgreetingHello(String str)
+    public static void testgreeTingsHello(String str)
     {
         System.out.println("Greetings, " + str + "! How are you?");
     }
  public static void main(String[] args) {
-     System.out.println("Hello, world!");
      
-     testgreetingHello("Briley");
-     testgreetingHello("Dr. Kessner");
+     testgreeTingsHello("Briley");
+     testgreeTingsHello("Dr. Kessner"); 
  }
 }
--- unit4/reverseFunction.java
+++ unit4/reverseFunction.java
@@ -2,7 +2,7 @@ public class reverseFunction {
     public static String reverseString(String str) {
         String result = "";
         for (int i = str.length() - 1; i >= 0; i--) {
-            result += str.charAt(i);
+            result += str.substring(i, i + 1);
         }
         return result;
     }
