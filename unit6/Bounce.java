05ee7f2183d740ed3e21b4b0a2b053f118868a4f
bfranceee <brileydfrance@gmail.com>
2024-11-20 14:58:22 -0800

    y

---
+++ unit6/Bounce.java
@@ -0,0 +1,37 @@
+import processing.core.*;
+import java.util.*;   // for ArrayList
+
+
+public class Bounce extends PApplet
+{
+    public void settings()
+    {
+        size(400, 400);
+    }
+
+    public void setup()
+    {
+        balls = new ArrayList<Ball>();
+        balls.add(new Ball(this));
+    }
+
+    public void draw()
+    {
+        background(256,0,0);
+        for (Ball b : balls)
+            b.display();
+    }
+
+    public void keyPressed()
+    {
+        for (int i=0; i<10; i++)
+            balls.add(new Ball(this));        
+    }
+
+    private ArrayList<Ball> balls;
+
+    public static void main(String[] args)
+    {
+        PApplet.main("Bounce");
+    }
+}
