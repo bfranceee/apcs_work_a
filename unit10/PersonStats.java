27b0f1aea9993133bca9cbfb71edd104b958828b
bfranceee <brileydfrance@gmail.com>
2025-02-07 07:56:25 -0800

    m

--- unit10/PersonStats.java
+++ unit10/PersonStats.java
@@ -1,7 +1,7 @@
-import java.util.*; 
+import java.util.*;
 
-public class PersonStats{
-     public static double averagePet(ArrayList<Person> pets) {
+public class PersonStats {
+    public static double averagePet(ArrayList<Person> pets) {
         double sum = 0;
 
         for (Person a : pets) {
@@ -10,8 +10,8 @@ public class PersonStats{
 
         return sum / pets.size();
     }
-    
- public static Person mostPets (ArrayList<Person> pets) {
+
+    public static Person mostPets(ArrayList<Person> pets) {
         Person toomanypets = pets.get(0);
 
         for (Person a : pets) {
@@ -21,10 +21,30 @@ public class PersonStats{
         }
         return toomanypets;
     }
-public static Person twoPets (ArrayList<Person> pets) {
-    for (Person a : pets) {
-            if (a.getPets() >= 2) {
-                return person;
+
+    public static List<Person> peopleWithAtLeastTwoPets(ArrayList<Person> pets) {
+        List<Person> result = new ArrayList<>();
+
+        for (Person a : pets) {
+            if (a.getPet() >= 2) { 
+                result.add(a); 
             }
         }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<Person> pets = new ArrayList<>();
+
+        pets.add(new Person("John", 2));
+        pets.add(new Person("Piper", 4));
+        pets.add(new Person("Molly", 6));
+
+        System.out.println("Average number of pets: " + averagePet(pets));
+        System.out.println("Person with most pets: " + mostPets(pets).getName());
+        System.out.println("People with at least two pets:");
+        for (Person person : peopleWithAtLeastTwoPets(pets)) {
+            System.out.println(person.getName());
+        }
+    }
 }
